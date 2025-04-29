418ae6fa7d89c367a0391568f0ca3e7ad584a746
Ralph <ralph.sleigh@gmail.com>
2025-04-29 14:19:19 +0100

    Try this

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,42 +1,27 @@
 resource "aws_api_gateway_rest_api" "gateway" {
   name = "Bookings API Gateway ${terraform.workspace}"
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "Bookings API Gateway ${terraform.workspace}"
-      version = "1.0"
-    }
+}
 
-    paths = {
-      "/{proxy+}" = {
-        "ANY" = {
-          operationId = "proxy"
-          parameters = [
-            {
-              name     = "proxy"
-              in       = "path"
-              required = true
-              schema   = {
-                type = "string"
-              }
-            }
-          ]
-          responses = {
-            default = {
-              description = "default response"
-            }
-          }
-          x-amazon-apigateway-integration = {
-            uri         = aws_lambda_function.api_lambda.invoke_arn
-            httpMethod  = "POST"
-            type        = "aws_proxy"
-          }
-        }
-      }
-    }
-    
-    name = "Bookings API Gateway ${terraform.workspace}"
-  })
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "{proxy+}"
+  parent_id   = aws_api_gateway_rest_api.gateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.gateway.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_api.invoke_arn
 }
 
 /*
