c5697a2d63ef9ee53e6772aaf535cb740c58b58c
Ralph <ralph.sleigh@gmail.com>
2025-05-21 13:42:14 +0100

    Fixes

--- src/front/src/components/suspense.tsx
+++ src/front/src/components/suspense.tsx
@@ -2,5 +2,7 @@ import { Center, Loader } from "@mantine/core";
 import React from "react";
 
 export const SuspenseWrapper: React.FC<React.PropsWithChildren> = ({children}) => {
-    return <Center h={"100vh"}><Loader size="xl"/></Center>
+    return <Center h={"100vh"}>
+        <img src="/newspinner.svg" alt="Logo" width="100px"/>
+    </Center>
 }
--- src/local-server.ts
+++ src/local-server.ts
@@ -12,6 +12,15 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(jsonParser)
 app.use(urlencodedParser)
 
+app.use((req, res, next) => {    //runs for every path. Same as .use('/',
+  //'/hello' has NOT been trimmed from req.url
+  //req.url is /hello if the request was for /hello
+  if (req.url.startsWith('/api')) { 
+    req.url = req.url.substring(4); //remove /api from the start of the url
+  }
+  next(); //will continue calling all middleware
+});
+
 //@ts-expect-error
 app.get('*', wrapLambda(handler, {region: 'eu-west-2', timeoutInSeconds: 3600}));
 //@ts-expect-error
