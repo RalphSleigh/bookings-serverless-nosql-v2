e0c66e2896d47850640a64e81211ec100eec6fbd
Ralph <ralph.sleigh@gmail.com>
2025-12-09 16:47:21 +0000

    chunky checkbox

--- src/front/src/components/booking-form/permission.tsx
+++ src/front/src/components/booking-form/permission.tsx
@@ -3,6 +3,8 @@ import { Dispatch, SetStateAction } from 'react'
 
 import { TEvent } from '../../../../shared/schemas/event'
 
+import classes from '../../css/permissionCheckbox.module.css'
+
 export const PermissionForm = ({ event, checked, setChecked }: { event: TEvent; checked: boolean; setChecked: Dispatch<SetStateAction<boolean>> }) => {
   return (
     <>
@@ -10,17 +12,26 @@ export const PermissionForm = ({ event, checked, setChecked }: { event: TEvent; 
         Permission
       </Title>
       <Grid onClick={() => setChecked(!checked)} style={{ cursor: 'pointer' }}>
-        <Grid.Col span={11}>
-          <Text m={8} ml={16}>I give permission for the people named above to attend {event.name}.</Text>
-          <Text m={8} ml={16}>I acknowledge it is my responsibility to ensure everyone over 16 attending has up-to-date Woodcraft Folk membership and completed a DBS check.</Text>
+        <Grid.Col span={12}>
+          <Checkbox
+            size="xl"
+            p={8}
+            checked={checked}
+            onChange={(event) => setChecked(event.currentTarget.checked)}
+            m={8}
+            className={classes.root}
+          />
           <Text m={8} ml={16}>
-            I agree this information will be stored electronically and shared only with individuals who need this information to engage your child safely in Woodcraft Folk activities. Based on the needs
-            of your child we may also share any relevant information with medical or child protection professionals. For more information please visit{' '}
+            I give permission for the people named above to attend {event.name}.
+          </Text>
+          <Text m={8} ml={16}>
+            I acknowledge it is my responsibility to ensure everyone over 16 attending has up-to-date Woodcraft Folk membership and completed a DBS check.
+          </Text>
+          <Text m={8} ml={16}>
+            I agree this information will be stored electronically and shared only with individuals who need this information to engage your child safely in Woodcraft Folk activities. Based on the
+            needs of your child we may also share any relevant information with medical or child protection professionals. For more information please visit{' '}
             <a href="https://www.woodcraft.org.uk/privacy">www.woodcraft.org.uk/privacy</a> or contact <a href="mailto:data@woodcraft.org.uk">data@woodcraft.org.uk</a>
           </Text>
-        </Grid.Col>
-        <Grid.Col span={1}>
-          <Checkbox checked={checked} onChange={(event) => setChecked(event.currentTarget.checked)} m={8} />
         </Grid.Col>
       </Grid>
     </>
